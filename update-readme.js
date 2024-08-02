d6f3376d1b029f6f4668d47be7309d959372d758
jangjinhong <honge1122@naver.com>
2024-08-02 16:04:05 +0900

    file update

--- update-readme.js
+++ update-readme.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+import { writeFileSync } from 'fs';
 
 // README 파일 경로
 const readmePath = './README.md';
@@ -6,7 +6,7 @@ const readmePath = './README.md';
 // README 업데이트 로직
 const updateReadme = () => {
   const newContent = '# Updated README\n\nThis is an updated README file.';
-  fs.writeFileSync(readmePath, newContent, 'utf8');
+  writeFileSync(readmePath, newContent, 'utf8');
   console.log('README.md has been updated');
 };
 
