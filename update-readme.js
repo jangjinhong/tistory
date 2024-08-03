27689a35f5724e0915a2a527aa7b4613e2f99e4c
jangjinhong <honge1122@naver.com>
2024-08-03 12:38:41 +0900

    commit error update

--- update-readme.js
+++ update-readme.js
@@ -5,7 +5,7 @@ const readmePath = './README.md';
 
 // README 업데이트 로직
 const updateReadme = () => {
-  const newContent = '# Updated README\n\nThis is an updated README file.';
+  const newContent = `# Updated README\n\nThis is an updated README file. Last updated at ${new Date().toISOString()}`;
   writeFileSync(readmePath, newContent, 'utf8');
   console.log('README.md has been updated');
 };
